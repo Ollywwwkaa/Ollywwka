9148e6a1e13f81e099323f2513d939ef9581c7ce
BogdanovaOY <kkssrrzz929@gmail.com>
2025-10-09 18:56:58 +0500

    New files

--- Отчёты по ЛР/Отчет по лабораторной работе №5/1.py
+++ Отчёты по ЛР/Отчет по лабораторной работе №5/1.py
@@ -1,4 +1,7 @@
 a = int(input("Введите число"))
 b = int(input("Введите второе число.."))
-result = a * b
-print ("Результат:", result)
+summ = a * b
+dell  = a / b
+razn = a - b
+umn = a * b
+print ("Результат:", summ,dell,razn,umn)
