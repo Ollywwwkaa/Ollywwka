5e1baa1deed2e6b0979c5f2ba0335f2daf6432c1
BogdanovaOY <kkssrrzz929@gmail.com>
2025-10-28 12:05:37 +0500

    New ex

--- Отчёты по ЛР/Deadline_2/Отчёт по лабораторной работе №9/4.py
+++ Отчёты по ЛР/Deadline_2/Отчёт по лабораторной работе №9/4.py
@@ -1,8 +1,8 @@
-n = int(input("Введите высоту: "))
-for i in range(1, n + 1):  
-    print(' ' * (n - i), end='')  
-for j in range(1, i + 1):  
-    print(j, end='')   
-for j in range(i - 1, 0, -1):  
-    print(j, end='')    
-print()  
+n = int(input("Введите высоту пирамиды: "))
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+    for j in range(1, i + 1):
+        print(j, end="")
+    for j in range(i - 1, 0, -1):
+        print(j, end="")
+    print()
