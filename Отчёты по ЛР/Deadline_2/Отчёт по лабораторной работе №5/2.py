7c9a1ba210d9a7e1f0ef515432d4774ef88fd2dd
BogdanovaOY <kkssrrzz929@gmail.com>
2025-11-09 13:27:16 +0500

    New

---
+++ Отчёты по ЛР/Deadline_2/Отчёт по лабораторной работе №5/2.py
@@ -0,0 +1,22 @@
+# 1-2. Создаем список кортежей с информацией о студентах
+students = [
+    ("Анна", 21, 4.5),
+    ("Петр", 22, 4.2),
+    ("Мария", 19, 4.8),
+    ("Иван", 20, 4.1),
+    ("Елена", 23, 4.6)]
+for student in students:
+    print(f"- {student[0]} ({student[1]} лет), средний балл: {student[2]}")
+print("\nстарше 20 лет:")
+for name, age, srball in students:
+    if age > 20:
+        print(f"- {name} ({age}), средний балл: {srball}")
+stud = students[0]
+for student in students:
+    if student[2] > stud[2]:
+        stud = student
+print(f"\nЛучший: {stud[0]}, средний балл: {stud[2]}")
+sort = sorted(students, key=lambda x: x[0])
+print("\nСтуденты отсортированные по имени:")
+for name, age, srball in sort:
+    print(f"- {name} ({age} лет), средний балл: {srball}")
