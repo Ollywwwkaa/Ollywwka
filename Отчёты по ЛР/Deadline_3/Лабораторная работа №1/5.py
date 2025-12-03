8501026dc9a5f86c4a5009691f6acd162d197328
BogdanovaOY <kkssrrzz929@gmail.com>
2025-12-03 15:28:41 +0500

    New

---
+++ Отчёты по ЛР/Deadline_3/Лабораторная работа №1/5.py
@@ -0,0 +1,14 @@
+def safe_exec(func):
+    def wrapper(a, b):
+        try:
+            result = func(a, b)
+            return result
+        except ZeroDivisionError:
+            print("Ошибка: Деление на ноль!")
+            return 0
+    return wrapper
+@safe_exec
+def divide(a, b):
+    return a / b
+print(f"Результат 1: {divide(10, 2)}")
+print(f"Результат 2: {divide(5, 0)}")
