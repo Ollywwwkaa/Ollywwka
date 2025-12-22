edcc011f173ca737c6d9a5f02bb00525f944aa1a
Ollywwwkaa <kkssrrzz929@gmail.com>
2025-12-22 17:36:30 +0500

    Add files via upload

--- Отчёты по ЛР/KR_OAIP/KR_3/1.py
+++ Отчёты по ЛР/KR_OAIP/KR_3/1.py
@@ -1,38 +1,28 @@
-def audit_log(func):
-    def wrapper(self, amount):
-        print(f'Выполнена операция "{func.__name__}". Сумма: {amount}')
-        return func(self, amount)
-    return wrapper
+def audit_log(operation_name):
+    def decorator(fun):
+        def wrapper(self, summ):
+            print(f'Операция "{operation_name}" на сумму: {summ}')
+            return fun(self, summ)
+        return wrapper
+    return decorator
 class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.__balance = initial_balance
-    @audit_log
-    def deposit(self, amount):
-        if amount <= 0:
-            print("Сумма для пополнения должна быть положительной")
-            return False
-        self.__balance += amount
-        return True
-    @audit_log
-    def withdraw(self, amount):
-        if amount <= 0:
-            print("Сумма для снятия должна быть положительной")
-            return False
-        if amount > self.__balance:
-            print("Недостаточно средств на счете")
-            return False
-        self.__balance -= amount
-        return True
-    def balance(self):
-        return self.__balance
-if __name__ == "__main__":
-    account = BankAccount(1000)
-    print(f"Начальный баланс: {account.balance()}\n")
-    account.deposit(500)
-    print(f"Баланс после пополнения: {account.balance()}\n")
-    account.withdraw(300)
-    account.withdraw(1500)
-    print(f"Баланс после попытки снятия: {account.balance()}\n")
-    account.deposit(-100)
-    account.withdraw(0)
-    print(f"Итоговый баланс: {account.balance()}")
+    balance = 0
+    @audit_log("пополнение")
+    def deposit(self, summ):
+        self.balance += summ
+    @audit_log("снятие")
+    def withdraw(self, summ):
+        if summ <= self.balance:
+            self.balance -= summ
+        else:
+            print("Недостаточно денег")
+account = BankAccount()
+print(f"Баланс: {account.balance}")
+account.deposit(100000)
+print(f"Баланс: {account.balance}")
+account.withdraw(300)
+print(f"Баланс: {account.balance}")
+account.withdraw(800000)
+print(f"Баланс: {account.balance}")
+account.withdraw(800)
+print(f"Баланс: {account.balance}")
